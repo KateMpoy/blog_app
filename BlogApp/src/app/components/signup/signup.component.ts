653ec7f1bac6f3a097814de0cdebf49dde0fa560
KateMpoy <mpoyntakidoy2009@gmail.com>
2020-09-20 20:41:34 +0300

    update

--- BlogApp/src/app/components/signup/signup.component.ts
+++ BlogApp/src/app/components/signup/signup.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
+
 export class SignupComponent implements OnInit {
+  signupForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    public fb: FormBuilder,
+    public router: Router
+  ) {
+    this.signupForm = this.fb.group({
+      name: [''],
+      email: [''],
+      mobile: [''],
+      password: ['']
+    })
   }
 
+  ngOnInit() { }
+
+  registerUser() {
+    // this.authService.signUp(this.signupForm.value).subscribe((res) => {
+    //   if (res.result) {
+    //     this.signupForm.reset()
+    //     this.router.navigate(['log-in']);
+    //   }
+    // })
+  }
 }
